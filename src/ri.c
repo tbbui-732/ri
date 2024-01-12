16e16cd6c1ff9b19403b5839965788a226467706
thienbaobui <80117447+tbbui-732@users.noreply.github.com>
2024-01-12 18:43:42 -0500

    feat: write functions associated with raw mode

--- src/ri.c
+++ src/ri.c
@@ -1,7 +1,33 @@
+/* --- Libraries --- */
 #include <stdio.h>
 
-// TODO: set terminal from canonical to raw mode
+/* --- Program Failure --- */
+void die(char* die_message) {
+    
+}
 
+/* --- Terminal Specific --- */
+void turnRawModeOn(void) {
+    /* 
+     * Sets terminal to raw mode. 
+     * Raw mode reads values from stdin 
+     * immediately.
+     */
+
+
+}
+
+void turnRawModeOff(void) {
+    /*
+     * Sets terminal back to canonical 
+     * mode. 
+     *
+     * Set when exiting the program or 
+     * when a failure occurs
+     */
+}
+
+/* --- Main --- */
 int main(void) {
     printf("Hello world");
     return 0;
