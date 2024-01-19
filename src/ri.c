4010dacb14c3248a52f77ed4eb717000e2fd14bd
thienbaobui <80117447+tbbui-732@users.noreply.github.com>
2024-01-19 15:23:32 -0500

    refactor: Remove rawMode functions in favor of ncurses

--- src/ri.c
+++ src/ri.c
@@ -23,46 +23,6 @@ void die(char* message)
 }
 
 
-/* --- Terminal Specific --- */
-void turnRawModeOn(void) 
-{
-    /* 
-     * Sets terminal to raw mode. 
-     * Raw mode reads values from stdin 
-     * immediately.
-     */
-
-    // Get the default terminal settings
-    tcgetattr(STDOUT_FILENO, &GDATA.default_term);
-
-    // Create termios struct to store raw mode settings
-    struct termios rawmode_settings = GDATA.default_term;
-
-    // Set flags to make terminal "raw"
-    rawmode_settings.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-    rawmode_settings.c_oflag &= ~(OPOST);
-    rawmode_settings.c_cflag |= (CS8);
-    rawmode_settings.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-
-    // Set terminal to raw mode
-    tcsetattr(STDOUT_FILENO, TCSAFLUSH, &rawmode_settings);
-}
-
-void turnRawModeOff(void) 
-{
-    /*
-     * Sets terminal back to canonical 
-     * mode. 
-     *
-     * Set when exiting the program or 
-     * when a failure occurs
-     */
-
-    // Set terminal back to default settings
-    tcsetattr(STDOUT_FILENO, TCSAFLUSH, &GDATA.default_term);
-}
-
-
 /* --- Input --- */
 enum keys {
     UP_ARROW = 65,
