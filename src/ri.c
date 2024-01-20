f6a85cbba0b716b6d4c89b4b2ffd519f902da143
thienbaobui <80117447+tbbui-732@users.noreply.github.com>
2024-01-20 17:36:59 -0500

    feat: remove keymap

--- src/ri.c
+++ src/ri.c
@@ -4,15 +4,15 @@
 #define KEY_CTRL(key) ((key) & 0x1F)
 
 /* --- Data --- */
-struct global_data {
+struct global_data
+{
     struct termios default_term;
 };
 
 struct global_data GDATA;
 
-
 /* --- Program Failure --- */
-void die(char* message) 
+void die(char *message)
 {
     /*
      * Write error message to standard output
@@ -22,36 +22,61 @@ void die(char* message)
     refresh();
     endwin();
 
-    printf("%s\n", message);    
+    printf("%s\n", message);
     exit(1);
 }
 
-
 /* --- Input --- */
-enum keys {
-    UP_ARROW = 65,
-    DOWN_ARROW,
-    RIGHT_ARROW,
-    LEFT_ARROW
-};
-
-void processUserInput(void) 
+void processUserInput(void)
 {
-    char ch = getch();
-    while (ch != KEY_CTRL('q')) {
-        attron(A_BOLD);
-        printw("%d, %c\n", ch, ch);
-        attroff(A_BOLD);
-        ch = getch();
+    int ch;
+    ch = getch();
+    while (ch != KEY_CTRL('q'))
+    {
+        printw("%d\n", ch);
+        // if (ch == KEY_UP)
+        // {
+        //     printw("going up\n");
+        //     getch();
+        // }
+        // else if (ch == KEY_DOWN)
+        // {
+        //     printw("going down\n");
+        //     getch();
+        // }
+        // else if (ch == KEY_LEFT)
+        // {
+        //     printw("going left\n");
+        //     getch();
+        // }
+        // else if (ch == KEY_RIGHT)
+        // {
+        //     printw("going right\n");
+        //     getch();
+        // }
+        // else if (ch == KEY_HOME)
+        // {
+        //     printw("going home\n");
+        //     getch();
+        // }
+        // else if (ch == KEY_LL)
+        // {
+        //     printw("going end\n");
+        //     getch();
+        // }
+        // else {
+        //     printw("%d", ch);
+        //     getch();
+        // }
     }
+
     refresh();
 }
 
-
 /* --- Main --- */
-int main(void) 
+int main(void)
 {
-    initscr();              // Initialize ncurses
+    initscr(); // Initialize ncurses
     raw();
     noecho();
     keypad(stdscr, TRUE);
@@ -59,7 +84,7 @@ int main(void)
     processUserInput();
 
     refresh();
-    endwin();               // Kill ncurses
+    endwin(); // Kill ncurses
 
     return 0;
 }
