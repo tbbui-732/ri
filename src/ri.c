4d77c66f012c000f3c77789e52182b99c185e7f3
thienbaobui <80117447+tbbui-732@users.noreply.github.com>
2024-01-26 21:51:29 -0500

    refactor: Moved contents of prototype header into main file

--- src/ri.c
+++ src/ri.c
@@ -1,4 +1,24 @@
-#include "prototypes.h"
+/* --- Libraries --- */
+#include <stdlib.h>
+#include <sys/termios.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <ncurses.h>
+#include <math.h>
+
+/* --- Constants --- */
+#define TURN_ON 2000
+#define TURN_OFF 2001
+#define ESC_SEQ 27
+
+/* --- Definitions --- */
+#define KEY_CTRL(key) ((key) & 0x1F)
+// #define KEY_END 360
+#define KEY_PGUP 339
+#define KEY_PGDN 338
+#define VBAR_WIDTH 3            // arbitrary value for now
+#define STATUS_BAR_HEIGHT 3     // arbitrary value for now
+
 
 /* --- Global Data --- */
 struct globalData
@@ -114,9 +134,9 @@ void initializeGlobalData(void)
     getmaxyx(stdscr, term_nrows, term_ncols);
 
     editor.vbar = initializeNewWindow(term_nrows - STATUS_BAR_HEIGHT, // Initialize vertical bar
-                                      VBAR_WIDTH,
-                                      0,
-                                      0);
+            VBAR_WIDTH,
+            0,
+            0);
 
     // TODO; Once the vertical bar works, come back to these windows
     // initializeNewWindow(&editor.screen,                                  // Initialize text-editor screen
