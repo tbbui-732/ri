d277079b50352565eea2bfbbf5b4cacd0b0fb293
thienbaobui <80117447+tbbui-732@users.noreply.github.com>
2024-02-12 00:34:16 -0500

    feat: restart file bc main file is too messy; currently have tilde draw and process input

---
+++ src/reri.c
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ncurses.h>
+
+/* definitions */
+#define KEY_CTRL(key) ((key) & 0x1F)
+#define KEY_PGUP 339
+#define KEY_PGDN 338
+
+/* global methods */
+void die(char *message) {
+    refresh();
+    endwin();
+    fprintf(stderr, "%s\n", message);
+    exit(1);
+}
+
+/* user interface related methods */
+void draw_tildes(void) {
+    int screen_height, y_axis;
+    screen_height = getmaxy(stdscr);
+    for (y_axis = 0; y_axis < screen_height; y_axis++) {
+        mvprintw(y_axis, 0, "~");
+    }
+    refresh();
+}
+
+/* input related methods */
+void process_user_input(void) {
+    int ch;
+    ch = getch();
+    while (ch != KEY_CTRL('q')) {
+        int ypos, xpos; // cursor position
+        int max_y, max_x; // screen dimension
+        getyx(stdscr, ypos, xpos);
+        getmaxyx(stdscr, max_y, max_x);
+
+        if (ch == KEY_UP || ch == 'k')          --ypos;
+        else if (ch == KEY_DOWN  || ch == 'j')  ++ypos;
+        else if (ch == KEY_LEFT  || ch == 'h')  --xpos;
+        else if (ch == KEY_RIGHT || ch == 'l')  ++xpos;
+        else if (ch == KEY_HOME)                xpos = 0;
+        else if (ch == KEY_END)                 xpos = max_x - 1;
+        else if (ch == KEY_PGUP)                ypos = 0;
+        else if (ch == KEY_PGDN)                ypos = max_y - 1;
+        else printw("%c\n", ch);
+
+        move(ypos, xpos);
+        ch = getch();
+    }
+    refresh();
+}
+
+/* start here */
+int main(int argc, char *argv[]) {
+
+    // initialize ncurses here
+    initscr();
+    raw(); 
+    noecho();
+    keypad(stdscr, TRUE);
+
+    // user defined methods go here
+    draw_tildes();
+    process_user_input();
+
+    // kill ncurses here
+    refresh();
+    endwin();
+    return 0;
+}
