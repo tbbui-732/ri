6c17abfb1e391201e927773fc8216eeb4e6cfad1
thienbaobui <80117447+tbbui-732@users.noreply.github.com>
2024-02-12 13:00:33 -0500

    feat: successfully generated windows for status bar and side bar

--- src/reri.c
+++ src/reri.c
@@ -15,20 +15,32 @@ void die(char *message) {
     exit(1);
 }
 
+/* window creation and deletion methods */
+WINDOW *create_curse_window(int win_height, int win_width, int start_y, int start_x) {
+    WINDOW *new_window;
+    new_window = newwin(win_height, win_width, start_y, start_x);
+    box(new_window, 0, 0);
+    wrefresh(new_window);
+    return new_window;
+}
+
+void destroy_curse_window(WINDOW *local_win) {
+    wborder(local_win, ' ', ' ' , ' ', ' ', ' ', ' ', ' ', ' ');
+    wrefresh(local_win);
+    delwin(local_win);
+}
+
 /* user interface related methods */
 void draw_tildes(void) {
-    int screen_height, y_axis;
-    screen_height = getmaxy(stdscr);
-    for (y_axis = 0; y_axis < screen_height-1; y_axis++) { // NOTE: screen_height - 1 to account for status bar
+    int y_axis;
+    for (y_axis = 0; y_axis < LINES-1; y_axis++) { // NOTE: screen_height - 1 to account for status bar
         mvprintw(y_axis, 0, "~");
     }
     refresh();
 }
 
 void display_status_bar(void) {
-    int screen_height;
-    screen_height = getmaxy(stdscr);
-    mvprintw(screen_height-1, 0, "<normal mode>"); // TODO: make this dynamic in the future
+    mvprintw(LINES-1, 0, "<normal mode>"); // TODO: make this dynamic in the future
     move(0, 1);
     refresh();
 }
@@ -54,6 +66,7 @@ void process_user_input(void) {
         else printw("%c\n", ch);
 
         move(ypos, xpos);
+        refresh();
         ch = getch();
     }
     refresh();
@@ -68,30 +81,39 @@ int main(int argc, char *argv[]) {
     noecho();
     keypad(stdscr, TRUE);
 
-    // create window for sidebar (location for line number/tildes) and status bar (modes, writes, etc)
+    // Create window for sidebar (location for line number/tildes) and status bar (modes, writes, etc)
     WINDOW *side_bar, *status_bar;
-    int screen_height = getmaxy(stdscr);
-    int screen_width = getmaxx(stdscr);
+    const int STATUS_BOTTOM_PAD = 2;
+    const int SIDE_BAR_WIDTH = 2;
 
-    // TODO: why are my screens/windows not showing?!!!!!!
-    side_bar = newwin(screen_height, 1, 0, 0); // dim: screen height, 1 width | start: top-left corner
-    status_bar = newwin(1, screen_width, screen_height - 1, 0); // dim: 1 height, screen width | start: bottom-left corner
-    
-    // TODO: remove once windows actually work
-    box(side_bar, 0, 0);
-    box(status_bar, 0, 0);
-    wrefresh(side_bar);
-    wrefresh(status_bar);
+    // Must refresh stdscr before new windows can appear!
+    refresh(); 
 
-    // user defined methods go here
+    // Initialize sidebar window
+    side_bar = create_curse_window(
+            LINES - STATUS_BOTTOM_PAD, // STATUS_BOTTOM_PAD accounts for status_bar height
+            SIDE_BAR_WIDTH,
+            0, 
+            0); 
+
+    // Initialize status bar window
+    status_bar = create_curse_window(
+            STATUS_BOTTOM_PAD,
+            COLS,
+            LINES - STATUS_BOTTOM_PAD,
+            0);
+
+    move(0, SIDE_BAR_WIDTH);
+
+    // User defined methods go here
     // draw_tildes(); // TODO: uncomment this code once figure out why screens are not showing
     // display_status_bar();
     process_user_input();
 
-    // kill ncurses here
+    // Kill all ncurses related structures here
+    destroy_curse_window(side_bar);
+    destroy_curse_window(status_bar);
     refresh();
-    delwin(side_bar);
-    delwin(status_bar);
     endwin();
     return 0;
 }
